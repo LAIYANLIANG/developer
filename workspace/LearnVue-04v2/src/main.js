5062556ec9728aeef9b70e4edee6ffddb3ad4dbb
LAIYANLIANG <windsonglove@gmail.com>
2021-10-08 17:50:25 +0800

    20211008

---
+++ workspace/LearnVue-04v2/src/main.js
@@ -0,0 +1,3 @@
+const {add,mul}=require('./mathUtils.js')
+console.log('第一個');
+console.log('二個');
--- workspace/LearnVue-04v2/src/mathUtils.js
+++ workspace/LearnVue-04v2/src/mathUtils.js
@@ -5,3 +5,7 @@ function add(num1,num2){
 function mul(num1,num2){
     return num1*num2;
 }
+module.exports={
+    add,
+    mul
+}
