856f33ab1de4aa9c7e669716bcc86e16de828d91
LAIYANLIANG <windsonglove@gmail.com>
2021-10-13 00:11:37 +0800

    Revert "20211008"
    This reverts commit 5062556ec9728aeef9b70e4edee6ffddb3ad4dbb.

--- workspace/LearnVue-04v2/src/main.js
+++
@@ -1,3 +0,0 @@
-const {add,mul}=require('./mathUtils.js')
-console.log('第一個');
-console.log('二個');
--- workspace/LearnVue-04v2/src/mathUtils.js
+++ workspace/LearnVue-04v2/src/mathUtils.js
@@ -5,7 +5,3 @@ function add(num1,num2){
 function mul(num1,num2){
     return num1*num2;
 }
-module.exports={
-    add,
-    mul
-}
