905b03699b7397064153ca03e461c369d91947ae
LAIYANLIANG <windsonglove@gmail.com>
2021-10-24 20:23:25 +0800

    202110242023

--- chapter01/part3/webpack.config.js
+++ chapter01/part3/webpack.config.js
@@ -1,7 +1,10 @@
 //引入一個包
 //path就是nodeJs中的一個模塊，直接引入就好~~
 const path = require('path');
-
+//引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+//引入clean插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 //webpack中的所有的配置信息，都應該寫在module.exports中
 module.exports = {
@@ -20,15 +23,29 @@ module.exports = {
     //指定webpack打包時要使用的模塊
     module: {
         //指定要加載的規則
-        rules:[
+        rules: [
             {
                 //test指定的是規則生效的文件
-                test:/\.ts$/,
+                test: /\.ts$/,
                 //要使用的loader
-                use:'ts-loader',
+                use: 'ts-loader',
                 //要排除的文件
-                exclude:/node-modules/
+                exclude: /node-modules/
             }
         ]
+    },
+
+    //配置webpack插件
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin(
+            {
+                //title:"這是一個自定義的title"
+                template: "./src/index.html"
+            }),
+    ],
+    //用來設引用模塊
+    resolve: {
+        extensions: ['.ts', '.js']
     }
-}
+};
